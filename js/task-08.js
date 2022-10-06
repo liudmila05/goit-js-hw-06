5176a8fa263e1eca2a415dc652b95f56c403f32d
liudmila05 <ol.liuda.94@gmail.com>
2022-10-06 19:54:50 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,13 +5,13 @@ form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
     event.preventDefault();
     const {
-        elements: { email, password },
+        elements: {email, password},
     } = event.currentTarget;
 
     if (email.value === '' || password.value === '') {
         return alert('Please fill in all the fields!');
     };
-    const userDetails = { email: email.value, Password: password.value};
+    const userDetails = {email: email.value, Password: password.value};
     
     console.log(userDetails);
     event.currentTarget.reset();
